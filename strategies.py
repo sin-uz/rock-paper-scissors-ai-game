bdb7454b9eb88591d1663915dc3ecb34beea91ca
MKunik <mateusz.kunik1996@gmail.com>
2026-02-20 10:19:39 +0100

    add a new tactic to research based strategy

--- strategies.py
+++ strategies.py
@@ -1,50 +1,44 @@
 import random as rd
 
-from domain import Move, Outcome, MOVE_BEATS
+from domain import Move, Outcome, MOVE_BEATS, MOVE_LOSES
 
 
 class NaiveStrategy:
-    def __init__(self, action_space=tuple(Move)):
-        self.action_space = action_space
-
-    def reset(self):
-        pass
-    
-    def select_move(self):
-        return rd.choice(self.action_space)
-    
-
-class ResearchBasedStrategy:
-
-    def __init__(
-            self,
-            action_space=tuple(Move),
-            action_weights=[0.40, 0.35, 0.25]
-    ):
-        self.action_space = action_space
+    def __init__(self, action_weights: list = [0.25, 0.40, 0.35]):
         self.action_weights = action_weights
 
     def select_move(self, match_history=None):
-        # level 0
+        if not match_history:
+            return self.first_move()
+    
+    def first_move(self):
+        return rd.choices(tuple(Move), self.action_weights, k=1)[0]
+    
+
+class ResearchBasedStrategy(NaiveStrategy):
+
+    def select_move(self, match_history=None):
         if not match_history:
             return self.first_move()
         
-        round_number = match_history[-1].round_number
+        previous_round =  self._get_previous_round(match_history)
+        last_three_moves = self._get_last_moves(match_history)[-3:]
 
-        # level 1
-        previous_round =  match_history[-1]
-
+        if self._are_moves_same(last_three_moves):
+            return MOVE_LOSES[previous_round.player_move]
+        
         if previous_round.outcome == Outcome.PLAYER:
             return MOVE_BEATS[previous_round.computer_move]
         elif previous_round.outcome == Outcome.COMPUTER:
             return previous_round.player_move
-        else:
-            return self.first_move()
+    
+        return self.first_move()
         
-        # level 2
-        #
-        #
-        #
-
-    def first_move(self):
-        return rd.choices(self.action_space, self.action_weights, k=1)[0]
+    def _get_previous_round(self, match_history):
+        return match_history[-1]    
+    
+    def _get_last_moves(self, match_history):
+        return [round.player_move.value for round in match_history]
+    
+    def _are_moves_same(self, moves):
+        return len(set(moves)) == 1
